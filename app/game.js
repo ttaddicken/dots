5ad6045df81fb5e28247cb9f5735af0b84ad79d6
Timothy L Taddicken <Timothy L Taddicken>
2016-03-01 01:20:18 -0700

    names added for players

--- app/game.js
+++ app/game.js
@@ -1,22 +1,18 @@
 angular.module('con4', [])
     .controller('GameController', function ($scope) {
         $scope.grid = [];
-        $scope.background = "teal"
+        $scope.startup = true
+// Start Game 
         $scope.newGame = function () {
             $scope.victory = false
+            $scope.currentplayer = ""
             $scope.activePlayer = $scope.player1Color
             $scope.grid = buildGrid();
         }
-
+// End start game
+// build grid
         function buildGrid() {
-
-			/**
-			 * Cell Schema
-			 * {
-			 * 		row: number,
-			 * 		col: number
-			 * }
-			 */
+            $scope.startup = false
             $scope.grid = []
             for (var i = 0; i < 6; i++) {
                 var row = i
@@ -29,8 +25,9 @@ angular.module('con4', [])
             console.log($scope.grid);
             return $scope.grid
         }
+// end build grid
         //  this is being passed from the UI "dropToken(cell.col)"
-
+// this function is for dropping the game token
         $scope.dropToken = function (col) {
             if ($scope.victory) { return }
             debugger;
@@ -52,28 +49,14 @@ angular.module('con4', [])
             var cell = $scope.grid[row][col];
             cell.hasToken = true;
             cell.color = $scope.activePlayer
-			/**
-			 * Once the row is identified
-			 * set the cell by accessing 
-			 * $scope.grid[row][col]
-			 **/
-            checkVictory(cell);
+			checkVictory(cell);
             endTurn();
             //endTurn and checkVictory
         }
-
+// This function checks for available slots
         function checkSouth(row, col) {
             debugger
-            /**
-             * Let's use recursion
-             * A recursive function is...
-             * a function that calls itself
-             * until some condition is met
-             * 
-             * Check South will need essentially two base cases
-             * 
-             */
-			
+           
             //Base case 1 found south Token return row - 1 to go back one step
             if ($scope.grid[row][col].hasToken) {
                 row--
@@ -86,18 +69,9 @@ angular.module('con4', [])
             }
             row++
             return checkSouth(row, col)          
-			/**
-			 * if neither base case 
-			 * (***increment row***, then return checkSouth())
-			 * make sure to pass the arguments through
-			 */
-        }
-
+		}
+// this function calls victory
         function checkVictory(cell) {
-            //This one is a gimme you shouldn't have to change anything here
-            //Once you fix the checkNextCell function the green squiggles should dissapear.
-            //If they don't make sure you are returning a number from the checkNextCell function
-			
             var horizontalMatches = 0;
             //Check Horizontal
             horizontalMatches += checkNextCell(cell, 0, 'left');
@@ -123,23 +97,8 @@ angular.module('con4', [])
                 alert(cell.color + ' Wins');
             }
         }
-
+// This function checks the cells around the token cell
         function getNextCell(cell, direction) {
-			/**
-			 * var nextRow = cell.row;
-			 * var nextCol = cell.col;
-			 * 
-			 * adjust the values of nextRow
-			 * and nextCol as needed based upon
-			 * the direction of travel.
-			 * 
-			 * if nextRow > 0 or < 5 
-			 * or if nextCol > 6 
-			 * return null;
-			 * 
-			 * otherwise 
-			 * return $scope.grid[nextRow][nextCol];
-			 */
             var nextRow = cell.row;
             var nextCol = cell.col;
 
@@ -202,11 +161,13 @@ angular.module('con4', [])
         }
 
         function endTurn() {
-
+debugger;
             if ($scope.activePlayer === $scope.player1Color) {
-                $scope.activePlayer = $scope.player2Color
+                $scope.activePlayer = $scope.player2Color;
+                $scope.currentplayer = $scope.player2
             } else {
-                $scope.activePlayer = $scope.player1Color
+                $scope.activePlayer = $scope.player1Color;
+                $scope.currentplayer = $scope.player1
             }
         }
     });
